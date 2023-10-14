141c997bd2fdcc1cf602e1d54a9939aaa218e56f
Hugo Santos <hf.santos2332@gmail.com>
2023-10-14 12:30:47 -0300

    Cria repository de users

--- src/main/java/br/com/hugofsantos/todolist/user/UserModel.java
+++ src/main/java/br/com/hugofsantos/todolist/user/UserModel.java
@@ -1,10 +1,26 @@
 package br.com.hugofsantos.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data // Adicionar getters e setters
+@Entity(name = "tb_users")
 public class UserModel {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
   private String username;
   private String password;
   private String name;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
